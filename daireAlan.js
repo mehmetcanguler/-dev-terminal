b2520a770619a859bf51b43ff29eab6041e2821e
Mehmet Can GÜLER <91988777+mehmetcanguler@users.noreply.github.com>
2021-12-29 14:41:43 +0300

    daireAlan

---
+++ daireAlan.js
@@ -0,0 +1,7 @@
+const arguments = process.argv.slice(2);
+
+function daireAlan(r){
+    var alan=Math.PI*r*r
+    console.log("yarı çap= "+r+" dairenin alanı= "+alan)
+}
+daireAlan(arguments[0] *1,)
